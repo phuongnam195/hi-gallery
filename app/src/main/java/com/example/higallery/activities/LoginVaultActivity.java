74cbfb796992488f024fdc1d91ec15b867936f0d
Nguyen Van Loi <nguyenvanloi10101@gmail.com>
2021-10-11 16:22:55 +0700

    Thêm ripple cho nút bấm, Xử lí hiệu ứng và thông báo khi sai mật khẩu

--- app/src/main/java/com/example/higallery/activities/LoginVaultActivity.java
+++ app/src/main/java/com/example/higallery/activities/LoginVaultActivity.java
@@ -1,12 +1,19 @@
 package com.example.higallery.activities;
 
+import static com.example.higallery.R.anim.login_vault_vibrate_animation;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
+import com.example.higallery.MainActivity;
 import com.example.higallery.R;
 
 public class LoginVaultActivity extends Activity {
@@ -15,12 +22,21 @@ public class LoginVaultActivity extends Activity {
 
     private RadioButton[] dots = new RadioButton[PIN_LENGTH];
     private String currentPIN = "";
+    private TextView notificationLoginVault;
     private Button[] numkeys = new Button[10];
+
+    private Animation animationForRadioGroups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_vault);
+
+        //Add animation
+        animationForRadioGroups = AnimationUtils.loadAnimation(
+                getApplicationContext(),
+                login_vault_vibrate_animation
+        );
 
         //Get radio
         for (int i = 0; i < PIN_LENGTH; i++) {
@@ -28,6 +44,11 @@ public class LoginVaultActivity extends Activity {
             int resID = getResources().getIdentifier(radioID, "id", getPackageName());
             dots[i] = (RadioButton) findViewById(resID);
         }
+
+
+        //Get notification Textview
+        notificationLoginVault = (TextView) findViewById(R.id.notification_login_vault);
+        notificationLoginVault.setText(R.string.login_vault_subtitle);
 
         //Get and set event for keyboard
         for (int i = 0; i < 10; i++) {
@@ -45,8 +66,16 @@ public class LoginVaultActivity extends Activity {
                         return;
                     }
 
+                    //Status of radio group when input password
                     currentPIN = currentPIN + String.valueOf(num);
                     dots[currentPIN.length() - 1].setChecked(true);
+
+                    //Status of notification when input first word
+                    if(currentPIN.length() == 1){
+                        notificationLoginVault.setText("");
+                    }
+
+                    //Check password when input enough
                     if (currentPIN.length() == PIN_LENGTH) {
                         validatePIN();
                     }
@@ -61,10 +90,18 @@ public class LoginVaultActivity extends Activity {
             finish();
             startActivity(intent);
         } else {
+            //Add animation for radio group
+            LinearLayout radioGroup = (LinearLayout) findViewById(R.id.pin_login_vault);
+            radioGroup.startAnimation(animationForRadioGroups);
+
+            //Delete status of all dots
             currentPIN = "";
             for (int i = 0; i < PIN_LENGTH; i++) {
                 dots[i].setChecked(false);
             }
+
+            //Notification for user that "password fail"
+            notificationLoginVault.setText(R.string.login_vault_password_incorrect);
         }
     }
 
