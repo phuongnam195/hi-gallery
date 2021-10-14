d7e309309d8ba16ca6c9caeaffb750f240f31a50
phuongnam195 <phuongnam1952001@gmail.com>
2021-10-14 11:50:56 +0700

    Thêm các nút cho màn hình Photo (hiển thị ảnh lớn), menu appbar (edit, new alnum, vault, set as, details), bottom bar (share, favorite, trash)

---
+++ app/src/main/java/com/example/higallery/activities/PhotoActivity.java
@@ -0,0 +1,151 @@
+package com.example.higallery.activities;
+
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.higallery.Configuration;
+import com.example.higallery.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class PhotoActivity extends AppCompatActivity {
+    private ActionBar appBar;
+    private LinearLayout bottomBar;
+    private FloatingActionButton favoriteButton;
+
+    private boolean dummyFavorite = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.set(this);
+        setContentView(R.layout.activity_photo);
+
+        setupAppBar();
+        setupBottomBar();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ImageView imageDummy = (ImageView) findViewById(R.id.imageDummy);
+        imageDummy.setImageResource(R.drawable.ic_launcher_background);
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void setupAppBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar_photo);
+        setSupportActionBar(toolbar);
+        appBar = getSupportActionBar();
+
+        // add back arrow to appbar
+        appBar.setDisplayHomeAsUpEnabled(true);
+        appBar.setDisplayShowHomeEnabled(true);
+
+        // disable show/hide animation
+        appBar.setShowHideAnimationEnabled(false);
+    }
+
+    private void setupBottomBar() {
+        bottomBar = (LinearLayout) findViewById(R.id.bottombar_photo);
+        favoriteButton = (FloatingActionButton) findViewById(R.id.favorite_action_photo);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_photo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBack();
+                return true;
+            case R.id.edit_action_photo:
+                onEdit();
+                return true;
+            case R.id.new_album_action_photo:
+                onNewAlbum();
+                return true;
+            case R.id.secure_action_photo:
+                onSecure();
+                return true;
+            case R.id.set_as_action_photo:
+                onSetAs();
+                return true;
+            case R.id.details_action_photo:
+                onDetails();
+                return true;
+        }
+        return false;
+    }
+
+    public void toggleBarVisibility(View view) {
+        if (appBar.isShowing()) {
+            appBar.hide();
+            bottomBar.setVisibility(View.GONE);
+
+        } else {
+            appBar.show();
+            bottomBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void onBack() {
+        finish();
+    }
+
+    public void onEdit() {
+        Toast.makeText(this, "Sửa...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onNewAlbum() {
+        Toast.makeText(this, "Thêm vào album mới (tạo album)...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onSecure() {
+        Toast.makeText(this, "Thêm vào thư mục bí mật...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onSetAs() {
+        Toast.makeText(this, "Đặt làm hình nền, hình khóa...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onDetails() {
+        Toast.makeText(this, "Xem metadata...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onShare(View view) {
+        Toast.makeText(this, "Chia sẻ...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void toggleFavorite(View view) {
+        Resources resources = getResources();
+        if (dummyFavorite) {
+            favoriteButton.setImageResource(R.drawable.ic_baseline_favorite_border);
+            favoriteButton.setTooltipText(resources.getString(R.string.photo_favorite));
+        } else {
+            favoriteButton.setImageResource(R.drawable.ic_baseline_favorite);
+            favoriteButton.setTooltipText(resources.getString(R.string.photo_unfavorite));
+        }
+        dummyFavorite = !dummyFavorite;
+
+        Toast.makeText(this, "Thích/Bỏ thích...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onDelete(View view) {
+        Toast.makeText(this, "Xóa vào thùng rác", Toast.LENGTH_SHORT).show();
+    }
+}
